895cb67124605c6ed256d066fe23a546c09e85c4
Christian Hansen <christian.william.hansen@gmail.com>
2017-01-11 09:01:22 -0800

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,5 @@
-PLACE YOUR CODE HERE
+# I can't think of another way to do this, so here it is:
+df = pd.read_csv('faculty.csv')
+df.columns =[u'name', u'degree', u'title', u'email'] 
+#print(df.head())
+df['email'].to_csv('email.csv')
