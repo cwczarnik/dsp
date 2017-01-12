3dd8add5eb2efcf0d3b301824c8f6071d6f712e5
Christian Hansen <christian.william.hansen@gmail.com>
2017-01-11 19:12:16 -0800

    Update q7_lists.py

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -3,6 +3,11 @@
 
 
 def match_ends(words):
+    count = 0
+    for w in words:
+        if len(w) >=2 and w[0] == w[-1]:
+            count +=1
+    return(count)
     """
     Given a list of strings, return the count of the number of strings
     where the string length is 2 or more and the first and last chars
@@ -15,10 +20,20 @@ def match_ends(words):
     >>> match_ends(['aaa', 'be', 'abc', 'hello'])
     1
     """
-    raise NotImplementedError
 
 
 def front_x(words):
+    x_list = []
+    rest_list = []
+    for w in words:
+        if w[0]=='x':
+            x_list.append(w)
+        else:
+            rest_list.append(w)
+    x_sort = sorted(x_list)
+    rest_sort = sorted(rest_list)
+    return(x_sort + rest_sort)
+            
     """
     Given a list of strings, return a list with the strings in sorted
     order, except group all the strings that begin with 'x' first.
@@ -32,10 +47,10 @@ def front_x(words):
     >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
     ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
     """
-    raise NotImplementedError
 
 
 def sort_last(tuples):
+    return(sorted(tuples, key = lambda x: x[1]))
     """
     Given a list of non-empty tuples, return a list sorted in
     increasing order by the last element in each tuple.
@@ -49,10 +64,16 @@ def sort_last(tuples):
     >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
     [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
-    raise NotImplementedError
 
 
 def remove_adjacent(nums):
+    array = []
+    for n in range(len(nums)):
+        if len(array) == 0 or nums[n]!= array[-1]:
+            array.append(nums[n])
+    return(array)
+    
+    
     """
     Given a list of numbers, return a list where all adjacent equal
     elements have been reduced to a single element, so [1, 2, 2, 3]
@@ -68,10 +89,25 @@ def remove_adjacent(nums):
     >>> remove_adjacent([])
     []
     """
-    raise NotImplementedError
 
 
 def linear_merge(list1, list2):
+    
+    #with some help from online sources I have what's below. 
+    merged = []
+
+    while len(list1)!=0 and len(list2)!= 0:
+        if list1[0] < list2[0]:
+            merged.append(list1.pop(0))
+        else:
+            merged.append(list2.pop(0))
+            #adding all values that might be the same
+    return(merged + list1 + list2)
+#This does the same thing, but the time complexity is different.
+def linear_merge_1(list1,list2):
+    total_list = list1+list2
+    return(sorted(total_list))
+    
     """
     Given two lists sorted in increasing order, create and return a
     merged list of all the elements in sorted order. You may modify
